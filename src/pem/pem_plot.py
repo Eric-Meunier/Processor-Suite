f28a93602dfc608ef0023cc70b69aee7d59e3262
Eric-Meunier <eric.meunier10@gmail.com>
2019-04-04 21:33:57 -0400

    Updates to pem_plot

--- src/pem/pem_plot.py
+++ src/pem/pem_plot.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import re
 
+
 class PEMFileEditor:
     """
     Class for making edits to and generating plots from PEM_Files
@@ -34,13 +35,42 @@ class PEMFileEditor:
         return out
 
     def convert_stations(self, data):
-        stations = [d['Station'] for d in data]
+        """
+        Converts station names into numbers, producing a negative number of the station is S or W
+        :param data: EM data section of a PEM file
+        :return: List of integer station numbers
+        """
+        # TODO This probably belongs in pem_parser
+        stations = np.array([d['Station'] for d in data])
+        # stations = np.unique(stations)
+
+        converted_stations = []
+
         for station in stations:
-            if re.match("\d+(E|N)", station):
-                return int(re.findall("(\d+)\w",station))
-            # elif re.match("\d+(S|W)", station):
-            #     return int(-(station))
-        # return station
+
+            if re.match(r"\d+(S|W)", station):
+                converted_stations.append(-int(re.sub(r"\D","",station)))
+
+            else:
+                converted_stations.append(int(re.sub(r"\D","",station)))
+
+        return converted_stations
+
+    def get_components(self, data):
+        """
+        Retrieve the unique components of the survey file
+        :param data: EM data section of a PEM file
+        :return: List of string components
+        """
+        unique_components = []
+
+        for reading in data:
+            component = reading['Component']
+
+            if component not in unique_components:
+                unique_components.append(component)
+
+        return unique_components
 
 
     # def remove_ontime(self):
@@ -51,38 +81,40 @@ class PEMFileEditor:
     #     pprint(self.open_file(path))
 
 pem = PEMFileEditor()
-path = r'C:/Users/Eric/PycharmProjects/Crone/sample_files/2400NAv.PEM'
+path = r'C:/Users/Mortulo/PycharmProjects/Crone/sample_files/2400NAv.PEM'
 
 file = pem.open_file(path)
 header = file.get_header()
 units = file.get_tags()['Units']
-channels = header['NumChannels']
+number_channels = header['NumChannels']
 data = file.get_data()
-
-x_data = list(filter(lambda d: d['Component'] == 'X', data))
-z_data = list(filter(lambda d: d['Component'] == 'Z', data))
-z_stations = []
-# pprint(pem.convert_stations(z_data))
-station  = '10E'
-result = station.replace()
-pprint(result.group())
-pprint(type(result.group()))
-
-# pprint(z_stations)
-# stations = np.array(stations)
-# pprint(sorted(stations))
-# pprint(type(stations))
+components = pem.get_components(data)
+profile_data = {}
 
 
-# for station in stations:
-#     # if re.match(r'W|S',station):
-#     pprint(station)
+for component in components[0]:
+    filtered_data = list(filter(lambda d: d['Component'] == component, data))
+    # pprint(filtered_data)
+
+    for station in filtered_data:
+        # pprint(station['Data'])
+        pp = list(map(lambda x: x, station['Data']))
+        pprint(pp)
 
 
 
 
 
-#
+# def profile_data(data):
+#     components =
+#     profiled_data = []
+#     for station in stations:
+#         for channel in data:
+
+
+
+
+
 # fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5, 1, figsize=(8.5, 8))
 # pp = []
 # data_set = []
@@ -95,8 +127,8 @@ pprint(type(result.group()))
 #
 # fig2, ax1 = plt.subplots(1,1, figsize=(8.5, 11))
 #
-# ax1.plot(stations, data_set,'k',linewidth=0.6)
+# ax1.plot(z_stations, data_set,'k',linewidth=0.6)
 # plt.yscale('symlog')
 #
-# # plt.show()
+# plt.show()
 
