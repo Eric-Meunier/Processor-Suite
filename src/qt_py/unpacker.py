14770e53c406e41b27ada3ed8c5c680df52ef29c
Eric Meunier <eric.meunier10@gmail.com>
2019-12-19 11:38:35 -0500

    Unpacker can accept ZIP, RAR, and 7z files

--- src/qt_py/unpacker.py
+++ src/qt_py/unpacker.py
@@ -3,6 +3,8 @@ import os
 import re
 import csv
 import time
+from zipfile import ZipFile
+from pyunpack import Archive
 from shutil import copyfile, rmtree
 from pathlib import Path
 from PyQt5 import (QtCore, QtGui, uic)
@@ -31,63 +33,6 @@ def exception_hook(exctype, value, traceback):
 
 
 sys.excepthook = exception_hook
-
-
-class CustomTable(QTableWidget):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.setDragEnabled(True)
-        self.setAcceptDrops(True)
-        self.viewport().setAcceptDrops(True)
-        self.setDragDropOverwriteMode(False)
-        self.setDropIndicatorShown(True)
-
-        self.setSelectionMode(QAbstractItemView.ExtendedSelection)
-        self.setSelectionBehavior(QAbstractItemView.SelectRows)
-        self.setDragDropMode(QAbstractItemView.InternalMove)
-
-    def dropEvent(self, event: QtGui.QDropEvent):
-        if not event.isAccepted() and event.source() == self:
-            drop_row = self.drop_on(event)
-
-            rows = sorted(set(item.row() for item in self.selectedItems()))
-            rows_to_move = [
-                [QTableWidgetItem(self.item(row_index, column_index)) for column_index in range(self.columnCount())]
-                for row_index in rows]
-            for row_index in reversed(rows):
-                self.removeRow(row_index)
-                if row_index < drop_row:
-                    drop_row -= 1
-
-            for row_index, data in enumerate(rows_to_move):
-                row_index += drop_row
-                self.insertRow(row_index)
-                for column_index, column_data in enumerate(data):
-                    self.setItem(row_index, column_index, column_data)
-            event.accept()
-            for row_index in range(len(rows_to_move)):
-                self.item(drop_row + row_index, 0).setSelected(True)
-                self.item(drop_row + row_index, 1).setSelected(True)
-        super().dropEvent(event)
-
-    def drop_on(self, event):
-        index = self.indexAt(event.pos())
-        if not index.isValid():
-            return self.rowCount()
-
-        return index.row() + 1 if self.is_below(event.pos(), index) else index.row()
-
-    def is_below(self, pos, index):
-        rect = self.visualRect(index)
-        margin = 2
-        if pos.y() - rect.top() < margin:
-            return False
-        elif rect.bottom() - pos.y() < margin:
-            return True
-        # noinspection PyTypeChecker
-        return rect.contains(pos, True) and not (
-                    int(self.model().flags(index)) & QtCore.Qt.ItemIsDropEnabled) and pos.y() >= rect.center().y()
 
 
 class Unpacker(QMainWindow, Ui_UnpackerCreator):
@@ -203,12 +148,29 @@ class Unpacker(QMainWindow, Ui_UnpackerCreator):
                 if os.path.isdir(url):
                     e.accept()
                     return
+                elif url.lower().endswith('zip') or url.lower().endswith('7z') or url.lower().endswith('rar'):
+                    e.accept()
+                    return
         e.ignore()
 
     def dropEvent(self, e):
-        urls = [url.toLocalFile() for url in e.mimeData().urls()]
-        print(f"Opening {urls[0]}")
-        self.open_folder(urls[0])
+        url = [url.toLocalFile() for url in e.mimeData().urls()][0]
+
+        if os.path.isdir(url):
+            self.open_folder(url)
+        elif url.lower().endswith('zip') or url.lower().endswith('7z') or url.lower().endswith('rar'):
+            print(f"Extracting {os.path.basename(url)}")
+            extract_dir = os.path.dirname(url)
+            new_folder_dir = os.path.splitext(url)[0]
+
+            Archive(url).extractall(extract_dir)
+            self.open_folder(new_folder_dir)
+            # with ZipFile(url, mode='r') as zip:
+            #     print(f"Extracting {os.path.basename(url)}")
+            #     extract_dir = os.path.dirname(url)
+            #     zip.extractall(extract_dir)
+            #     new_dir = os.path.splitext(url)[0]
+            #     self.open_folder(new_dir)
 
     def format_widgets(self):
         self.dump_table.setColumnWidth(0, 200)
@@ -326,7 +288,7 @@ class Unpacker(QMainWindow, Ui_UnpackerCreator):
                                 additional_path = os.path.join(additional_folder, new_file_name)
                                 copyfile(old_path, additional_path)
                         try:
-                            print(f"Renaming \"{old_path}\" to \"{new_path}\"")
+                            print(f"Moving \"{old_path}\" to \"{new_path}\"")
                             copyfile(old_path, new_path)
                         except FileExistsError:
                             self.error.showMessage(f'\"{new_path}\" exists already')
@@ -364,7 +326,8 @@ def main():
     up.move(app.desktop().screen().rect().center() - up.rect().center())
     up.show()
     folder = r'C:\Users\Eric\PycharmProjects\Crone\sample_files\PEMGetter files\MRC-111\DUMP\Oct 31st 2019'
-    up.open_folder(folder)
+    zip_file = r'C:\Users\Eric\PycharmProjects\Crone\sample_files\PEMGetter files\MRC-111\DUMP\Oct 31st 2019.zip'
+    # up.open_folder(folder)
 
     sys.exit(app.exec())
 
