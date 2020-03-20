5b0916a30806a8cc55b8ef84f424425bacdedbef
Eric-Meunier <eric.meunier10@gmail.com>
2020-03-20 13:03:07 -0400

    Loop corner coordinates actually working correctly.

--- src/pem/pem_planner.py
+++ src/pem/pem_planner.py
@@ -10,7 +10,6 @@ from PyQt5 import QtGui, QtCore, uic
 from PyQt5.QtWidgets import (QApplication, QMainWindow)
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-from matplotlib import pyplot as plt
 from src.mag_field.mag_field_calculator import MagneticFieldCalculator
 
 sys._excepthook = sys.excepthook
@@ -74,9 +73,7 @@ class LoopPlanner(QMainWindow, Ui_LoopPlannerWindow):
         self.loop_angle_edit.returnPressed.connect(self.change_loop_angle)
 
         self.hole_easting_edit.returnPressed.connect(self.plot_hole)
-        self.hole_easting_edit.returnPressed.connect(lambda: self.shift_loop(int(self.hole_easting_edit.text()), 0))
         self.hole_northing_edit.returnPressed.connect(self.plot_hole)
-        self.hole_northing_edit.returnPressed.connect(lambda: self.shift_loop(0, int(self.hole_northing_edit.text())))
         self.hole_az_edit.returnPressed.connect(self.plot_hole)
         self.hole_dip_edit.returnPressed.connect(self.plot_hole)
         self.hole_length_edit.returnPressed.connect(self.plot_hole)
@@ -106,9 +103,8 @@ class LoopPlanner(QMainWindow, Ui_LoopPlannerWindow):
         self.hole_trace_plot = pg.PlotDataItem()
         # self.hole_trace_plot.showGrid()
         self.hole_collar_plot = pg.ScatterPlotItem()
-        self.section_extent_center = pg.ScatterPlotItem()
         self.section_extent_line = pg.PlotDataItem()
-        self.cs = None
+        self.loop_plot = pg.ScatterPlotItem()
 
         self.setup_plan_view()
         self.setup_section_view()
@@ -131,8 +127,7 @@ class LoopPlanner(QMainWindow, Ui_LoopPlannerWindow):
             return x, y, z
 
         def get_section_extents():
-            # Remove the previously plotted center and section line
-            self.section_extent_center.clear()
+            # Remove the previously plotted section line
             self.section_extent_line.clear()
 
             x, y, z = get_hole_projection()
@@ -154,12 +149,10 @@ class LoopPlanner(QMainWindow, Ui_LoopPlannerWindow):
             p1 = (line_center_x - dx, line_center_y - dy)
             p2 = (line_center_x + dx, line_center_y + dy)
 
-            # Plot the center point and section line
-            self.section_extent_center = pg.ScatterPlotItem([line_center_x], [line_center_y], width=3, pen='r')
-            self.section_extent_line = pg.PlotDataItem([line_center_x - dx, line_center_x + dx],
+            # Plot the section line
+            self.section_extent_line.setData([line_center_x - dx, line_center_x + dx],
                                                        [line_center_y - dy, line_center_y + dy], width=1,
                                                        pen=pg.mkPen(color=0.5, style=QtCore.Qt.DashLine))
-            self.plan_view_vb.addItem(self.section_extent_center)
             self.plan_view_vb.addItem(self.section_extent_line)
             return p1, p2
 
@@ -193,7 +186,6 @@ class LoopPlanner(QMainWindow, Ui_LoopPlannerWindow):
             self.ax.plot(plotx, plotz, color='dimgray', lw=1)
 
         def plot_mag(c1, c2):
-
             wire_coords = self.get_loop_coords()
             mag_calculator = MagneticFieldCalculator(wire_coords)
             xx, yy, zz, uproj, vproj, wproj, plotx, plotz, arrow_len = mag_calculator.get_2d_magnetic_field(c1, c2)
@@ -201,16 +193,20 @@ class LoopPlanner(QMainWindow, Ui_LoopPlannerWindow):
                            units='dots', scale=.050, width=.8, headlength=11, headwidth=6)
 
         def plot_trace(xs, ys):
-            self.hole_trace_plot = pg.PlotDataItem(xs, ys, pen=pg.mkPen(width=2, color=0.5))
-            self.hole_collar_plot = pg.ScatterPlotItem([self.hole_easting], [self.hole_northing],
+            self.hole_trace_plot.setData(xs, ys, pen=pg.mkPen(width=2, color=0.5))
+            self.hole_collar_plot.setData([self.hole_easting], [self.hole_northing],
                                                        pen=pg.mkPen(width=3, color=0.5))
             self.plan_view_vb.addItem(self.hole_trace_plot)
             self.plan_view_vb.addItem(self.hole_collar_plot)
 
-        # self.hole_easting = 0
-        self.hole_easting = int(self.hole_easting_edit.text())
-        # self.hole_northing = 0
-        self.hole_northing = int(self.hole_northing_edit.text())
+        if int(self.hole_easting_edit.text()) != self.hole_easting:
+            shift_amt = int(self.hole_easting_edit.text()) - self.hole_easting
+            self.shift_loop(shift_amt, 0)
+            self.hole_easting = int(self.hole_easting_edit.text())
+        if int(self.hole_northing_edit.text()) != self.hole_northing:
+            shift_amt = int(self.hole_northing_edit.text()) - self.hole_northing
+            self.shift_loop(0, shift_amt)
+            self.hole_northing = int(self.hole_northing_edit.text())
         self.hole_az = int(self.hole_az_edit.text())
         self.hole_dip = -int(self.hole_dip_edit.text())
         self.hole_length = int(self.hole_length_edit.text())
@@ -246,7 +242,6 @@ class LoopPlanner(QMainWindow, Ui_LoopPlannerWindow):
             self.datumCBox.addItem(datum)
 
     def setup_plan_view(self):
-        # self.plan_view_vb = self.plan_view_widget.addViewBox(row=1, col=0, lockAspect=True)
         self.plan_view_vb = self.plan_view_widget.addPlot(row=1, col=0)
         self.plan_view_vb.showGrid(x=True, y=True, alpha=0.2)
         # self.plan_view_vb.disableAutoRange('xy')
@@ -274,7 +269,7 @@ class LoopPlanner(QMainWindow, Ui_LoopPlannerWindow):
         self.ax.spines['left'].set_visible(False)
         self.ax.spines['right'].set_visible(False)
         self.ax.spines['bottom'].set_visible(False)
-        # self.ax.get_xaxis().set_visible(False)
+        self.ax.get_xaxis().set_visible(False)
         self.ax.yaxis.set_major_formatter(ticker.StrMethodFormatter('{x:.0f}'))
         self.ax.figure.subplots_adjust(left=0.1, bottom=0.1, right=1., top=1.)
 
@@ -308,10 +303,6 @@ class LoopPlanner(QMainWindow, Ui_LoopPlannerWindow):
         self.loop_width_edit.setText(f"{w:.0f}")
         self.loop_height_edit.setText(f"{h:.0f}")
         self.loop_angle_edit.setText(f"{angle:.0f}")
-        # print(f"Lower left corner: {x}, {y}")
-        # print(f"Lower right corner: {x + w}, {y}")
-        # print(f"Upper Right corner: {x + w}, {y + h}")
-        # print(f"Upper left corner: {x}, {y + h}")
         self.loop_width_edit.blockSignals(False)
         self.loop_height_edit.blockSignals(False)
         self.loop_angle_edit.blockSignals(False)
@@ -319,8 +310,11 @@ class LoopPlanner(QMainWindow, Ui_LoopPlannerWindow):
         self.plot_hole()
 
     def shift_loop(self, dx, dy):
+        self.loop_roi.blockSignals(True)
         x, y = self.loop_roi.pos()
         self.loop_roi.setPos(x + dx, y + dy)
+        self.plan_view_vb.autoRange(items=[self.loop_roi])
+        self.loop_roi.blockSignals(False)
 
     def get_loop_coords(self):
         """
@@ -329,20 +323,19 @@ class LoopPlanner(QMainWindow, Ui_LoopPlannerWindow):
         """
         x, y = self.loop_roi.pos()
         w, h = self.loop_roi.size()
-        loop_handles = [(handle[1].x(), handle[1].y()) for handle in self.loop_roi.getSceneHandlePositions()[0:4]]
-        """
-        Looks like roi.getSceneHandlePositions returns coordinates tied to the graphics scene and not the plot 
-        coordinates so they are transformed into plot coordinates using the first corner (which is what is returned 
-        by roi.pos())
-        """
-        x_transform = x - loop_handles[0][0]
-        y_transform = y - loop_handles[0][1]
-        print(f"handle 1: {loop_handles[0][0] + x_transform, loop_handles[0][1] + y_transform}")
-        transformed_loop_corners = [(handle[0] + x_transform, handle[1] + y_transform, self.hole_elevation) for handle in loop_handles]
-        # transformed_loop_corners.append((loop_handles[0][0] + x_transform, loop_handles[0][1] + y_transform, self.hole_elevation))
-        return transformed_loop_corners
-        # return [(x, y, self.hole_elevation), (x + w, y, self.hole_elevation), (x + w, y + h, self.hole_elevation),
-        #         (x, y + h, self.hole_elevation)]
+        angle = self.loop_roi.angle()
+        c1 = (x, y, self.hole_elevation)
+        c2 = (c1[0] + w * (math.cos(math.radians(angle))), c1[1] + w * (math.sin(math.radians(angle))), self.hole_elevation)
+        c3 = (c2[0] - h * (math.sin(math.radians(angle))), c2[1] + h * (math.sin(math.radians(90-angle))), self.hole_elevation)
+        c4 = (c3[0] + w * (math.cos(math.radians(180-angle))), c3[1] - w * (math.sin(math.radians(180-angle))), self.hole_elevation)
+        corners = [c1, c2, c3, c4]
+
+        # self.loop_plot.clear()
+        # self.loop_plot.setData([coord[0] for coord in corners],
+        #                        [coord[1] for coord in corners], pen=pg.mkPen(width=3, color='r'))
+        # self.plan_view_vb.addItem(self.loop_plot)
+
+        return corners
 
 
 class LoopROI(pg.ROI):
@@ -366,8 +359,8 @@ if __name__ == '__main__':
     from src.pem.pem_getter import PEMGetter
 
     app = QApplication(sys.argv)
-    pem_getter = PEMGetter()
-    pem_files = pem_getter.get_pems()
+    # pem_getter = PEMGetter()
+    # pem_files = pem_getter.get_pems()
     # editor = PEMPlotEditor(pem_files[0])
     # editor.show()
     planner = LoopPlanner()
