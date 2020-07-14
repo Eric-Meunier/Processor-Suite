Fixed bug with batch importing RI files. Continued work on PP rotation.

src/gps/gps_editor.py
@@ -379,10 +379,11 @@ class BoreholeGeometry:
         self.collar = collar
         self.segments = segments
 
-    def get_projection(self, num_segments=None):
+    def get_projection(self, num_segments=None, stations=None):
         """
         Uses the segments to create a 3D projection of a borehole trace. Can be broken up into segments and interpolated.
         :param num_segments: Desired number of segments to be output
+        :param stations: list, stations to use for interpolation to ensure they are in the segments
         :return: pandas DataFrame: Projected easting, northing, elevation, and relative depth from collar
         """
         # Create the data frame
@@ -395,13 +396,17 @@ class BoreholeGeometry:
         segments = self.segments.get_segments().dropna()
 
         # Interpolate the segments
-        if num_segments:
+        if num_segments or stations:
             azimuths = segments.Azimuth.to_list()
             dips = segments.Dip.to_list()
             depths = segments.Depth.to_list()
 
             # Create the interpolated lists
-            interp_depths = np.linspace(depths[0], depths[-1], num_segments)
+            if stations:
+                interp_depths = sorted(np.unique(np.concatenate((depths, stations))))
+                num_segments = len(interp_depths)
+            else:
+                interp_depths = np.linspace(depths[0], depths[-1], num_segments)
             interp_az = np.interp(interp_depths, depths, azimuths)
             interp_dip = np.interp(interp_depths, depths, dips)
             interp_lens = np.subtract(interp_depths[1:], interp_depths[:-1])
@@ -416,6 +421,7 @@ class BoreholeGeometry:
                  inter_units,
                  interp_depths)
             ).T
+
         else:
             segments = segments.to_numpy()
 

src/mag_field/mag_field_calculator.py
@@ -89,24 +89,26 @@ class MagneticFieldCalculator:
         top = (Dot1 / r1 - Dot2 / r2) * u0 * amps
         bottom = (CrossSqrd * 4 * np.pi)
         factor = (top / bottom)
-        cross = cross[~np.isnan(factor)]  # new
-        factor = factor[~np.isnan(factor)]  # new
+
+        cross = cross[~np.isnan(factor)]  # filter out any NaN
+        factor = factor[~np.isnan(factor)]  # filter out any NaN
         factor = factor[..., np.newaxis]
 
         field = cross * factor
         field = np.sum(field, axis=0)
 
-        if out_units == 'nT':
-            field = field * (10 ** 9)
-        elif out_units == 'pT':
-            field = field * (10 ** 12)
-        elif out_units == 'nT/s':
-            if ramp is None:
-                raise ValueError('For units of nT/s, a ramp time (in seconds) must be given')
+        if out_units:
+            if out_units == 'nT':
+                field = field * (10 ** 9)
+            elif out_units == 'pT':
+                field = field * (10 ** 12)
+            elif out_units == 'nT/s':
+                if ramp is None:
+                    raise ValueError('For units of nT/s, a ramp time (in seconds) must be given')
+                else:
+                    field = (field * (10 ** 9)) / ramp
             else:
-                field = (field * (10 ** 9)) / ramp
-        else:
-            raise ValueError('Invalid output unit')
+                raise ValueError('Invalid output unit')
 
         return field[0], field[1], field[2]
 