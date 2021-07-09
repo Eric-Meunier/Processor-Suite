8090bf1412c0b63bd24780118ccecbe4e6048e7e
nyshieh <50374876+nyshieh@users.noreply.github.com>
2021-07-09 10:12:01 -0400

    Commenting pemdxf

--- src/pem/pem_dxf.py
+++ src/pem/pem_dxf.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 import src.sdxf as sdxf
 from src.pem.pem_file import PEMParser, PEMFile
 
@@ -7,9 +9,16 @@ class DXFDrawing:
         self.drawing = sdxf.Drawing()
         self.features = 0
 
-    def add_df(self, df, closed_poly: bool, **kwargs):
-        # Pandas series don't like the datatypes so we recast
+    def add_df(self, df: pd.DataFrame, closed_poly: bool, **kwargs):
+        """
+        Add a dataframe type object to a DXF
+        :param df: dataframe with .Easting and .Northing
+        :param closed_poly: whether or not to join the first and last points
+        :param kwargs: passthrough to sdxf calls
+        """
+        # sdxf doesn't like the Series datatypes so we recast
         xs, ys = df.Easting.to_numpy(), df.Northing.to_numpy()
+
         self.drawing.layers.append(sdxf.Layer(color=7))
         i = 1
         while i < len(xs):
@@ -20,6 +29,9 @@ class DXFDrawing:
             self.drawing.append(sdxf.Line(points=[(xs[0], ys[0]), (xs[-1], ys[-1])], **kwargs))
 
     def save_dxf(self, out_path: str):
+        """
+        Save the dxfs to out_path, does some error catching
+        """
         assert out_path.endswith(".dxf")
         self.drawing.saveas(out_path)
 
@@ -28,20 +40,30 @@ class PEMDXFDrawing(DXFDrawing):
     def __init__(self):
         super().__init__()
 
-    def add_loop(self, pem: PEMFile):
-        self.drawing.layers.append(sdxf.Layer(name=f'{pem.loop_name}', color=7))
+    def add_loop(self, pem: PEMFile, **kw):
+        """
+        Add the loop from a PEMFile classed object
+        :param pem: PEMFile to plot
+        :param kw: passthrough for the dxf calls
+        """
+        self.drawing.layers.append(sdxf.Layer(name=f'{pem.loop_name}', color=7, **kw))
         if pem.is_mmr():
             self.add_df(pem.loop.df, closed_poly=False, layer=f'{pem.loop_name}')
         else:
             self.add_df(pem.loop.df, closed_poly=True, layer=f'{pem.loop_name}')
 
-    def add_surveyline(self, pem: PEMFile):
-        self.drawing.layers.append(sdxf.Layer(name=f'{pem.line_name}', color=7))
+    def add_surveyline(self, pem: PEMFile, **kw):
+        """
+        Add the survey borehole or surface line from a PEMFile classed object
+        :param pem: PEMFile to plot
+        :param kw:
+        :return:
+        """
+        self.drawing.layers.append(sdxf.Layer(name=f'{pem.line_name}', color=7, **kw))
         if pem.is_borehole():
             self.add_df(pem.segments.df, closed_poly=False, layer=f'{pem.loop_name}')
         else:
             self.add_df(pem.line.df, closed_poly=False, layer=f'{pem.loop_name}')
-
 
 if __name__ == "__main__":
     from src.pem.pem_file import PEMParser, PEMFile
