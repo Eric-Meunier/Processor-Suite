Fixed auto coil area calculation. Fixed bug with auto-calculating coil area from a DMP file.

src/pem/pem_file.py
@@ -288,6 +288,7 @@ class PEMFile:
         self.normalized = header.get('Normalized')
         self.primary_field_value = header.get('Primary field value')
         self.coil_area = header.get('Coil area')
+        self.original_coil_area = header.get('Coil area')
         self.loop_polarity = header.get('Loop polarity')
         self.channel_times = channel_table
         self.number_of_channels = len(channel_table)
@@ -1361,14 +1362,21 @@ class PEMFile:
                 # Only include common stations. PP represents the measured data, XYZ are theoretical.
                 df = pd.concat([pp_data, theory_pp_data], axis=1).rename({pp_ch_num: 'PP'}, axis=1).dropna(axis=0)
                 theory_pp = df.loc[:, component].to_numpy()
-                pp = df.loc[:, 'PP'].to_numpy()
+                measured_pp = df.loc[:, 'PP'].to_numpy()
 
-                diff = sum(np.abs(theory_pp - pp))
-                reversed_diff = sum(np.abs(theory_pp - (pp * -1)))
-                # print(f"Component: {component}\nDiff: {diff}\nReversed Diff: {reversed_diff}\n")
-                if abs(reversed_diff) < abs(diff):
+                diff = np.abs(theory_pp - measured_pp)
+                reversed_diff = np.abs(theory_pp - (measured_pp * -1))
+
+                # Find how many diff values are greater in the flipped array than in the original. If more than half the
+                # array is larger in the original, than it is probably incorrect.
+                if len(np.where(reversed_diff > diff)) > len(diff) * 0.5:
                     logger.info(f"{self.filepath.name} {component} component may be reversed.")
                     reversed_components.append(component)
+
+                # print(f"Component: {component}\nDiff: {diff}\nReversed Diff: {reversed_diff}\n")
+                # if abs(reversed_diff) < abs(diff):
+                #     logger.info(f"{self.filepath.name} {component} component may be reversed.")
+                #     reversed_components.append(component)
         return reversed_components
 
     def auto_name_file(self):
@@ -1676,26 +1684,25 @@ class PEMFile:
     def scale_coil_area(self, coil_area):
         """
         Scale the data by a change in coil area
-        :param coil_area: int: new coil area
+        :param coil_area: float, new coil area
         :return: PEMFile object: self with data scaled
         """
         logger.info(f"Scaling coil area of {self.filepath.name} to {coil_area}.")
 
         new_coil_area = coil_area
-        assert isinstance(new_coil_area, int), "New coil area is not type int"
         old_coil_area = float(self.coil_area)
 
         scale_factor = float(old_coil_area / new_coil_area)
 
         self.data.Reading = self.data.Reading * scale_factor  # Vectorized
-        logger.info(f"{self.filepath.name} coil area scaled to {new_coil_area} from {old_coil_area}.")
+        logger.info(f"{self.filepath.name} coil area scaled to {new_coil_area:.1f} from {old_coil_area:.1f}.")
 
         self.coil_area = new_coil_area
 
         for note in self.notes:
             if "Data scaled by coil area change of" in note:
                 self.notes.remove(note)
-        self.notes.append(f'<HE3> Data scaled by coil area change of {self.original_coil_area}/{new_coil_area}')
+        self.notes.append(f'<HE3> Data scaled by coil area change of {self.original_coil_area:.1f}/{new_coil_area:.1f}')
         return self
 
     def scale_current(self, current):
@@ -1706,7 +1713,7 @@ class PEMFile:
         """
         new_current = current
         assert isinstance(new_current, float), "New current is not type float"
-        logger.info(f"Performing current change for {self.filepath.name} to {current}.")
+        logger.info(f"Performing current change for {self.filepath.name:.2f} to {current:.2f}.")
 
         old_current = float(self.current)
 
@@ -1718,7 +1725,7 @@ class PEMFile:
         for note in self.notes:
             if "Data scaled by current change of" in note:
                 self.notes.remove(note)
-        self.notes.append(f'<HE3> Data scaled by current change of {new_current}A/{self.original_current}A')
+        self.notes.append(f'<HE3> Data scaled by current change of {new_current:.2f}A/{self.original_current:.2f}A')
         return self
 
     def scale_by_factor(self, factor):
@@ -1732,11 +1739,11 @@ class PEMFile:
         scaled_factor = factor / (1 + self.total_scale_factor)
 
         self.data.Reading = self.data.Reading * (1 + scaled_factor)  # Vectorized
-        logger.info(f"{self.filepath.name} data scaled by factor of {(1 + scaled_factor)}.")
+        logger.info(f"{self.filepath.name} data scaled by factor of {(1 + scaled_factor):.2f}.")
 
         self.total_scale_factor += factor
 
-        self.notes.append(f'<HE3> Data scaled by factor of {1 + factor}')
+        self.notes.append(f'<HE3> Data scaled by factor of {1 + factor:.2f}')
         return self
 
     def mag_offset(self):
@@ -4142,7 +4149,8 @@ if __name__ == '__main__':
     pg = PEMGetter()
 
     # pem_file = pg.parse(r"C:\_Data\2021\Eastern\Sterling\SP-21-040\RAW\xy_1205.PEM")
-    pem_file = pg.parse(r"C:\_Data\2021\Nantou BF\Borehole\_PX21002\RAW\xy-px21002_1206.PEM")
+    # pem_file = pg.parse(r"C:\_Data\2021\Nantou BF\Borehole\_PX21002\RAW\xy-px21002_1206.PEM")
+    pem_file = pg.parse(r"C:\_Data\2021\TMC\Senc Resources\Loop 11\RAW\700e_1210.PEM")
     # txt_file = sample_folder.joinpath(r"Line GPS\KA800N_1027.txt")
     # line = SurveyLine(txt_file)
     # line.get_warnings(stations=pem_file.get_stations(converted=True, incl_deleted=False))
@@ -4166,7 +4174,7 @@ if __name__ == '__main__':
     # pem_file, _ = pem_file.prep_rotation()
     # pem_file.get_theory_pp()
     # pem_file.get_theory_data()
-    # pem_file.get_reversed_components()
+    pem_file.get_reversed_components()
     # pem_file.rotate(method="unrotate")
     # pem_file.rotate(method="unrotate")
     # pem_file.filepath = pem_file.filepath.with_name(pem_file.filepath.stem + "(unrotated)" + ".PEM")
@@ -4178,7 +4186,7 @@ if __name__ == '__main__':
     # pem_file.to_xyz()
     # pem_file, _ = pem_file.prep_rotation()
     # pem_file.mag_offset()
-    pem_file = pem_file.rotate(method=None, soa=2)
+    # pem_file = pem_file.rotate(method=None, soa=2)
     # pem_file = pem_file.rotate(method='unrotate', soa=10)
     # pem_file = pem_file.rotate(method="pp", soa=1)
     # rotated_pem = prep_pem.rotate('pp')

src/qt_py/pem_plot_editor.py
@@ -300,6 +300,35 @@ class PEMPlotEditor(QMainWindow, Ui_PEMPlotEditor):
             self.pem_file.rotate(method=None, soa=soa_delta)
             self.refresh_plots(components="all", preserve_selection=True)
 
+        def calculate_coil_area():
+            """
+            Automatically calculate the theoretical PP fit with the measure PP, and calculate the scaling to apply to
+            the coil area to correct for the difference. Only for PEM files with all GPS.
+            :return: None
+            """
+            if not self.pem_file.has_all_gps():
+                return
+
+            scalings = []
+            for component in self.pem_file.get_components():
+                # Profile data is already split, so use index 0 for the PP channel.
+                measured_pp = self.profile_data[self.profile_data.Component == component].groupby(
+                    "Station", as_index=False).mean().loc[:, 0].reset_index(drop=True)
+                theory_pp = self.theory_data.loc[:, component]
+
+                scaling = (measured_pp - theory_pp) / measured_pp
+                mean_scaling = np.mean(scaling)
+                scalings.append(mean_scaling)
+                print(f"Mean scaling for {component} component: {mean_scaling * 100:.2f}%")
+
+            # Scale using the coil area as it gives a baseline to reference with other surveys using the same gear
+            total_mean_scaling = np.mean(scalings)
+            new_coil_area = self.pem_file.coil_area + (self.pem_file.coil_area * total_mean_scaling)
+            self.coil_area_sbox.setValue(new_coil_area)
+            self.message.information(self, "New Coil Area", f"Coil area set to {new_coil_area:.1f} "
+                                                            f"({self.pem_file.coil_area * total_mean_scaling:.1f}).")
+            # self.pem_file.scale_coil_area(self.pem_file.coil_area + new_coil_area)
+
         # Actions
         self.select_all_action.activated.connect(select_all_stations)
 
@@ -347,6 +376,7 @@ class PEMPlotEditor(QMainWindow, Ui_PEMPlotEditor):
         self.flip_decay_btn.clicked.connect(lambda: self.flip_decays(source='decay'))
         self.zoom_to_offtime_btn.clicked.connect(self.zoom_to_offtime)
 
+        self.calculate_coil_area_btn.clicked.connect(calculate_coil_area)
         self.change_comp_profile_btn.clicked.connect(lambda: self.change_decay_component_dialog(source='profile'))
         self.change_profile_suffix_btn.clicked.connect(lambda: self.change_suffix_dialog(source='profile'))
         self.flip_profile_btn.clicked.connect(lambda: self.flip_decays(source='profile'))
@@ -626,6 +656,9 @@ class PEMPlotEditor(QMainWindow, Ui_PEMPlotEditor):
                 self.auto_clean_std_sbox.setValue(2)
             else:
                 self.auto_clean_std_sbox.setValue(1.5)
+
+        if not self.pem_file.has_all_gps():
+            self.calculate_coil_area_btn.setEnabled(False)
 
         # Set the channel max for the single profile plot and auto clean box
         self.last_offtime_channel = self.pem_file.get_offtime_channels().index[-1]
@@ -2928,7 +2961,8 @@ if __name__ == '__main__':
     # pem_file = pem_g.get_pems(folder="Raw Surface", file=r"Loop L\Final\100E.PEM")[0]
     # pem_file = pem_g.get_pems(folder="Raw Surface", file=r"Loop L\RAW\800E.PEM")[0]
     # pem_file = pem_g.get_pems(folder="Raw Surface", file=r"Loop L\RAW\1200E.PEM")[0]
-    pem_file = pem_g.get_pems(folder="Raw Surface", file=r"JHadid\RAW\400n.PEM")[0]
+    # pem_file = pem_g.get_pems(folder="Raw Surface", file=r"JHadid\RAW\400n.PEM")[0]
+    pem_file = pem_g.parse(r"C:\_Data\2021\TMC\Benz Mining\EM21-230\RAW\em21-230z_1206.PEM")
     # pem_file = pem_g.get_pems(folder="Minera", file="L11000N_6.PEM")[0]
 
     editor = PEMPlotEditor(darkmode=darkmode)